13367cabc9dad3498744f8b47d93652871e1df5d
Yago Costa Ayala <77995791+YagoAyala@users.noreply.github.com>
2023-10-29 19:25:34 -0300

    feat: created signup logic

--- server/src/controllers/authentication.controller.js
+++ server/src/controllers/authentication.controller.js
@@ -2,11 +2,11 @@ const AuthenticationService = require("../services/authentication.service")
 
 const signUpController = async (req, res) => {
     try {        
-        const {Email, Password} = req.body
-        const result = await AuthenticationService.signUpService(Email, Password)
-        res.status(200).send(result)
+        const {User, Username, Email, Password} = req.body;
+        const result = await AuthenticationService.signUpService(User, Username, Email, Password);
+        res.status(200).send(result);
     } catch (error) {
-        res.status(500).send(error.message)
+        res.status(500).send(error.message);
     }
 }
 
--- server/src/services/authentication.service.js
+++ server/src/services/authentication.service.js
@@ -1,20 +1,83 @@
-const { admin } = require("../../database/database") // yago
-const adminAuth = admin.auth()
+const crud = require("../crud/crud");
+const CONST = require("../helper/constants");
+const bcrypt = require('bcryptjs');
 
-const signUpService = async (email, password) => {
-    const userRecord = await adminAuth.createUser({
-      email,
-      password,
-    });
+const isPasswordSafe = (password) => {
+    const MIN_LENGTH = 8;
 
-    console.log(userRecord, "userRecord", email, password,"password")
+    if (password.length < MIN_LENGTH) {
+        throw new Error(`Password length must be higher than ${MIN_LENGTH}`);
+    }
 
-    const namespace = "yago"
+    if (!/[a-z]/.test(password)) {
+        throw new Error("Password must have a lower case letter");
+    }
 
-    const result = await adminAuth.setCustomUserClaims(userRecord.uid, { namespace });
-    console.log(result, "result")
-    return "X"
+    if (!/[A-Z]/.test(password)) {
+        throw new Error("Password must have an upper case letter");
+    }
+
+    if (!/[0-9]/.test(password)) {
+        throw new Error("Password must have a number");
+    }
+
+    if (!/[!@#$%^&*]/.test(password)) {
+        throw new Error("Password must have a special character");
+    }
+    
+};
+
+const validateUsername = (username) => {
+    if (!username || typeof username !== "string") {
+        throw new Error("Username is not valid or not a string");
+    }
 }
+
+const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (!emailRegex.test(email)) {
+        throw new Error("Email is not valid");
+    }
+}
+
+const ensureSignUpArguments = (username, email, password) => {
+    validateUsername(username);
+    validateEmail(email);
+    isPasswordSafe(password);
+};
+
+const checkUserExistence = async (user, property, value) => {
+    const filter = [{
+        [CONST.Filter.Property]: property,
+        [CONST.Filter.Operator]: CONST.Filter.Operators.Equal,
+        [CONST.Filter.Value]: value,
+    }];
+
+    const users = await crud.getByFilter(user, "User", filter);
+
+    if (users.length) {
+        throw new Error(`${property} already taken.`);
+    }
+};
+
+const signUpService = async (user, username, email, password) => {
+    ensureSignUpArguments(username, email, password);
+
+    await checkUserExistence(user, "Email", email);
+    await checkUserExistence(user, "Username", username);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    
+    const newUserEntity = {
+        Email: email,
+        Username: username,
+        Password: hashedPassword
+    };
+
+    return await crud.create(user, "User", newUserEntity);
+};
 
 const signInService = async (email, password) => {
     const userRecord = await adminAuth.signInWithEmailAndPassword(email, password);
